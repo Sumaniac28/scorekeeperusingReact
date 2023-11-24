3e72b12c10ea66bb938836b39dd7a6e84a631b5f
Sumit Grover <sumitgrover108@gmail.com>
2023-11-24 18:24:54 +0530

    Form update and displaying score with commentary

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-var score=0;
-var wicket =0;
-var ScoreTracker=[];
+// defining the variables
 
+let score=0;
+let wicket =0;
+let hit;
+let ScoreTracker=[];
+let reactRef=React.createRef();
+
+// function works on clicking scores buttons
 function scoreUpdate(num){
     if(wicket<10){
-        score+=num;
-        ScoreTracker.push(num);
+        hit=num;
     }
     // we are re rendering the App componentto make changes in UI
     rootElement.render(<App/>);
 }
 
+// function works on clicking wicket button
 function wicketUpdate(){
     if(wicket<10){
-        wicket++;
-        ScoreTracker.push('W');
+        hit='W';
     }
     rootElement.render(<App/>);
 }
 
+// Component for buttons
 const AppBtn=()=>{
     return (
-        <div>
+        <div id="AppBtn">
+                <button className="btn" onClick={()=>scoreUpdate(0)}>0</button>
                 <button className="btn" onClick={()=>scoreUpdate(1)}>1</button>
                 <button className="btn" onClick={()=>scoreUpdate(2)}>2</button>
                 <button className="btn" onClick={()=>scoreUpdate(3)}>3</button>
@@ -33,34 +39,63 @@ const AppBtn=()=>{
     )
 }
 
+// component to show scorecard with commentary on screen
 const Result=()=>{
     return (
         <div>
             {ScoreTracker.map((res,index)=>{
-                // to display the score in new line after every 6 balls
-                
-
-                return (
-                <>
-                {(index%6==0 && index!=0)?<br/>:null}
-                <span key={index}>{res}</span>&nbsp;&nbsp;&nbsp;
-                </>
-                )
-            })}
+               return <p key={index}>{res}</p>
+    })}
         </div>
     )
 }
 
+
+// function works on submitting the form
+const handleSubmit=(event)=>{
+    event.preventDefault();
+    if(wicket>=10){
+        return;
+    }
+    ScoreTracker.unshift(<span>{`${hit}, ${reactRef.current.value}`}</span>);
+    if(hit=='W'){
+        wicket++;
+    }else{
+        score+=hit;
+    }
+    hit=0;
+    reactRef.current.value="";
+    rootElement.render(<App/>);
+}
+
+// Component for form
+const ScoreForm=()=>{
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="Scoreinput" value={hit}></input>
+                <input type="text" id="Commentary" placeholder="Add a comment" ref={reactRef}></input>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
+
+// Our major App component
 const App=()=>{
     return (
         <>
             <h1>Score Display</h1>
             <h2>Score : {score}/{wicket}</h2>
             <AppBtn/>
+            <ScoreForm/>
+            <br/>
+            <hr/>
             <Result/>
         </>
     )
 }
 
+// creating root and rendering App component
 const rootElement=ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(<App/>);
